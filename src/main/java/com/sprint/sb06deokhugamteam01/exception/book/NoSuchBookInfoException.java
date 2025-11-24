9c18225a6e110076b0f09f210be4787a801053e2
김재원 <dumdum7@hs.ac.kr>
2025-11-24 16:08:22 +0900

    refactor: 예외명 변경

--- src/main/java/com/sprint/sb06deokhugamteam01/exception/book/NoSuchBookInfoException.java
+++
@@ -1,7 +0,0 @@
-package com.sprint.sb06deokhugamteam01.exception.book;
-
-public class NoSuchBookInfoException extends RuntimeException {
-    public NoSuchBookInfoException(String message) {
-        super(message);
-    }
-}
