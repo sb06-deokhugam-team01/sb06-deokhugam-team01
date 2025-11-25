f0e4b1df6dcd54ee62734e3a0187abea23a7d020
김재원 <dumdum7@hs.ac.kr>
2025-11-25 14:00:02 +0900

    feat: get 메소드 구현

--- src/main/java/com/sprint/sb06deokhugamteam01/service/book/BookServiceImpl.java
+++ src/main/java/com/sprint/sb06deokhugamteam01/service/book/BookServiceImpl.java
@@ -5,21 +5,34 @@ import com.sprint.sb06deokhugamteam01.dto.book.BookDto;
 import com.sprint.sb06deokhugamteam01.dto.book.request.BookUpdateRequest;
 import com.sprint.sb06deokhugamteam01.dto.book.request.PagingBookRequest;
 import com.sprint.sb06deokhugamteam01.dto.book.response.CursorPageResponseBookDto;
+import com.sprint.sb06deokhugamteam01.exception.book.AllReadyExistsIsbnException;
+import com.sprint.sb06deokhugamteam01.exception.book.NoSuchBookException;
+import com.sprint.sb06deokhugamteam01.mapper.book.BookMapper;
+import com.sprint.sb06deokhugamteam01.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.Nullable;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
+@Service
+@RequiredArgsConstructor
 public class BookServiceImpl implements  BookService {
+
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
     @Override
     public BookDto getBookById(UUID id) {
-        return null;
+        return bookMapper.toDto(bookRepository.findById(id)
+                .orElseThrow(() -> new NoSuchBookException("존재하지 않는 도서입니다.")));
     }
 
     @Override
     public BookDto getBookByIsbn(String isbn) {
-        return null;
+        return bookMapper.toDto(bookRepository.findByIsbn(isbn)
+                .orElseThrow(() -> new NoSuchBookException("존재하지 않는 도서입니다.")));
     }
 
     @Override
@@ -29,6 +42,13 @@ public class BookServiceImpl implements  BookService {
 
     @Override
     public BookDto createBook(BookCreateRequest bookCreateRequest, @Nullable MultipartFile file) {
+
+        if (bookRepository.existsByIsbn(bookCreateRequest.isbn())) {
+            throw new AllReadyExistsIsbnException("이미 존재하는 ISBN 입니다.");
+        }
+
+
+
         return null;
     }
 
