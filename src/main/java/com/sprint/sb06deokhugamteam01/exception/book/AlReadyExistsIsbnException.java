6c2fe47b2cd710c7aa384158d1827ebc1b9c006b
김재원 <dumdum7@hs.ac.kr>
2025-11-25 15:36:33 +0900

    Refactor: 예외명 변경

---
+++ src/main/java/com/sprint/sb06deokhugamteam01/exception/book/AlReadyExistsIsbnException.java
@@ -0,0 +1,9 @@
+package com.sprint.sb06deokhugamteam01.exception.book;
+
+import com.sprint.sb06deokhugamteam01.exception.RootException;
+
+public class AlReadyExistsIsbnException extends RootException {
+    public AlReadyExistsIsbnException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/sprint/sb06deokhugamteam01/exception/book/AllReadyExistsIsbnException.java
+++
@@ -1,7 +0,0 @@
-package com.sprint.sb06deokhugamteam01.exception.book;
-
-public class AllReadyExistsIsbnException extends RuntimeException {
-    public AllReadyExistsIsbnException(String message) {
-        super(message);
-    }
-}
