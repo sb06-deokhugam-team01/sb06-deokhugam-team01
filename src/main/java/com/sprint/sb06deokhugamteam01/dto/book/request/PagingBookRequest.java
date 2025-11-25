1427f2e44fb0297ed5e0bc6134254029b68abc39
김재원 <dumdum7@hs.ac.kr>
2025-11-25 17:00:12 +0900

    feat: orderBy enum 클래스 추가

--- src/main/java/com/sprint/sb06deokhugamteam01/dto/book/request/PagingBookRequest.java
+++ src/main/java/com/sprint/sb06deokhugamteam01/dto/book/request/PagingBookRequest.java
@@ -1,18 +1,29 @@
 package com.sprint.sb06deokhugamteam01.dto.book.request;
 
 import lombok.Builder;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Builder
 public record PagingBookRequest(
      String keyword,
-     String orderBy,
+     OrderBy orderBy,
      SortDirection sortDirection,
      String cursor,
      LocalDateTime after,
      Integer limit
 ) {
+
+    @RequiredArgsConstructor
+    public enum OrderBy {
+        TITLE("title"),
+        PUBLISHED_DATE("publishedDate"),
+        RATING("rating"),
+        REVIEW_COUNT("reviewCount");
+
+        private final String fieldName;
+    }
 
     public enum SortDirection {
         ASC,
