ad7579be048cd394b97a3c5970e0ca91c6b67821
김재원 <dumdum7@hs.ac.kr>
2025-11-28 17:11:14 +0900

    test: naver api 테스트 코드 작성

---
+++ src/test/java/com/sprint/sb06deokhugamteam01/service/book/NaverBookSearchServiceTest.java
@@ -0,0 +1,36 @@
+package com.sprint.sb06deokhugamteam01.service.book;
+
+import com.sprint.sb06deokhugamteam01.dto.book.BookDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("NaverBookSearchService 테스트")
+class NaverBookSearchServiceTest {
+
+    @InjectMocks
+    private NaverBookSearchService naverBookSearchService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    @DisplayName("ISBN으로 도서 검색 테스트")
+    void testSearchBookByIsbn() {
+
+        String testIsbn = "9788966262083"; // 예시 ISBN
+        BookDto book = naverBookSearchService.searchBookByIsbn(testIsbn);
+
+        assertNotNull(book);
+        assertEquals(testIsbn, book.isbn());
+
+    }
+
+}
