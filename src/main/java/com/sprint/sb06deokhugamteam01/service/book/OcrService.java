217bfb5ad94557d4c5e899023b4b17d16fd9dcb9
김재원 <dumdum7@hs.ac.kr>
2025-11-28 17:11:06 +0900

    feat: api 인터페이스 생성

--- src/main/java/com/sprint/sb06deokhugamteam01/service/book/OcrService.java
+++ src/main/java/com/sprint/sb06deokhugamteam01/service/book/OcrService.java
@@ -2,6 +2,6 @@ package com.sprint.sb06deokhugamteam01.service.book;
 
 public interface OcrService {
 
-    String extractIsbnFromImage(byte[] file, String fileType);
+    String extractIsbnFromImage(byte[] imageBytes, String fileType);
 
 }
