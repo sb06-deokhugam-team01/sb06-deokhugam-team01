cda3db55824a8146302e1601b110eb0e5b03b794
김재원 <dumdum7@hs.ac.kr>
2025-12-02 14:15:08 +0900

    feat: 프로필 추가

--- src/test/java/com/sprint/sb06deokhugamteam01/service/book/NaverBookSearchServiceTest.java
+++ src/test/java/com/sprint/sb06deokhugamteam01/service/book/NaverBookSearchServiceTest.java
@@ -8,10 +8,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
 @DisplayName("NaverBookSearchService 테스트")
 class NaverBookSearchServiceTest {
 
