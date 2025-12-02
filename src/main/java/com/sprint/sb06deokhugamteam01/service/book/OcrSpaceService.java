5dd00fe11c11716617b90326fbd35d4f9d465090
김재원 <dumdum7@hs.ac.kr>
2025-12-02 15:16:01 +0900

    feat: ocr space 추출 메소드 생성

--- src/main/java/com/sprint/sb06deokhugamteam01/service/book/OcrSpaceService.java
+++ src/main/java/com/sprint/sb06deokhugamteam01/service/book/OcrSpaceService.java
@@ -1,10 +1,15 @@
 package com.sprint.sb06deokhugamteam01.service.book;
 
+import com.sprint.sb06deokhugamteam01.exception.book.BookInfoFetchFailedException;
 import lombok.Builder;
 import org.springframework.web.client.RestClient;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class OcrSpaceService implements OcrService{
 
@@ -18,7 +23,7 @@ public class OcrSpaceService implements OcrService{
                 .baseUrl(ocrSpaceApiEndpoint)
                 .build();
 
-        String result = restClient.post()
+        OcrResult ocrResult = restClient.post()
                 .header("apikey", apiKey)
                 .body(ApiRequest.builder()
                         .apiKey(apiKey)
@@ -26,9 +31,34 @@ public class OcrSpaceService implements OcrService{
                         .fileType(fileType)
                         .build())
                 .retrieve()
-                .body(String.class);
+                .body(OcrResult.class);
 
-        return "";
+        if (ocrResult != null && ocrResult.ParsedResults().length > 0) {
+            String parsedText = ocrResult.ParsedResults()[0].ParsedText();
+            return parseIsbnFromOcrResult(parsedText);
+        } else {
+            throw new BookInfoFetchFailedException(detailMap("message", "OCR processing failed or returned no results."));
+        }
+    }
+
+    static Pattern ISBN_PATTERN = Pattern.compile("(97[89][0-9A-Za-z\\-•.]+)");
+
+    private String parseIsbnFromOcrResult(String input) {
+
+        Matcher matcher = ISBN_PATTERN.matcher(input);
+
+        List<String> list = new ArrayList<>();
+
+        while (matcher.find()) {
+            list.add(matcher.group(1));
+        }
+
+        if (list.isEmpty()) {
+            throw new BookInfoFetchFailedException(detailMap("message", "No ISBN found in OCR result."));
+        }
+
+        return list.get(0);
+
     }
 
     @Builder
@@ -39,6 +69,23 @@ public class OcrSpaceService implements OcrService{
     ){
 
     }
+
+    public record OcrResult(
+            ParsedResult[] ParsedResults,
+            int OCRExitCode,
+            boolean IsErroredOnProcessing,
+            double ProcessingTimeInMilliseconds,
+            String SearchablePDFURL
+    ) {}
+
+    public record ParsedResult(
+            String Overlay,
+            int FileParseExitCode,
+            String TextOrientation,
+            String ParsedText,
+            String ErrorMessage,
+            String ErrorDetails
+    ) {}
 
     private Map<String, Object> detailMap(String key, Object value) {
         Map<String, Object> details = new HashMap<>();
