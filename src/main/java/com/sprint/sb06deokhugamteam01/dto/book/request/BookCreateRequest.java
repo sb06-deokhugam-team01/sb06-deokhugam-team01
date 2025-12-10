d64a7540a02ccf4949f66211a00fc4aed9c887ee
김재원 <dumdum7@hs.ac.kr>
2025-12-10 14:02:31 +0900

    feat: 제약조건 255자 제한 적용

--- src/main/java/com/sprint/sb06deokhugamteam01/dto/book/request/BookCreateRequest.java
+++ src/main/java/com/sprint/sb06deokhugamteam01/dto/book/request/BookCreateRequest.java
@@ -1,5 +1,6 @@
 package com.sprint.sb06deokhugamteam01.dto.book.request;
 
+import jakarta.validation.constraints.Max;
 import lombok.Builder;
 import com.sprint.sb06deokhugamteam01.domain.book.Book;
 import jakarta.validation.constraints.NotBlank;
@@ -10,16 +11,20 @@ import java.time.LocalDate;
 @Builder
 public record BookCreateRequest(
         @NotBlank(message = "제목은 비어 있을 수 없습니다.")
+        @Max(value = 255, message = "제목은 최대 255자까지 입력할 수 있습니다.")
         String title,
         @NotBlank(message = "저자는 비어 있을 수 없습니다.")
+        @Max(value = 255, message = "저자는 최대 255자까지 입력할 수 있습니다.")
         String author,
         @NotNull(message = "설명은 비어 있을 수 없습니다.")
         String description,
         @NotBlank(message = "출판사는 비어 있을 수 없습니다.")
+        @Max(value = 255, message = "출판사는 최대 255자까지 입력할 수 있습니다.")
         String publisher,
         @NotNull(message = "출판일은 비어 있을 수 없습니다.")
         LocalDate publishedDate,
         @NotBlank(message = "ISBN은 비어 있을 수 없습니다.")
+        @Max(value = 20, message = "ISBN은 최대 20자까지 입력할 수 있습니다.")
         String isbn
 ) {
 
--- src/main/java/com/sprint/sb06deokhugamteam01/dto/book/request/BookUpdateRequest.java
+++ src/main/java/com/sprint/sb06deokhugamteam01/dto/book/request/BookUpdateRequest.java
@@ -1,6 +1,7 @@
 package com.sprint.sb06deokhugamteam01.dto.book.request;
 
 import com.sprint.sb06deokhugamteam01.domain.book.Book;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
@@ -10,12 +11,15 @@ import java.time.LocalDate;
 @Builder
 public record BookUpdateRequest(
     @NotBlank(message = "제목은 비어 있을 수 없습니다.")
+    @Max(value = 255, message = "제목은 최대 255자까지 입력할 수 있습니다.")
     String title,
     @NotBlank(message = "저자는 비어 있을 수 없습니다.")
+    @Max(value = 255, message = "저자는 최대 255자까지 입력할 수 있습니다.")
     String author,
     @NotNull(message = "설명은 비어 있을 수 없습니다.")
     String description,
     @NotBlank(message = "출판사는 비어 있을 수 없습니다.")
+    @Max(value = 255, message = "출판사는 최대 255자까지 입력할 수 있습니다.")
     String publisher,
     @NotNull(message = "출판일은 비어 있을 수 없습니다.")
     LocalDate publishedDate
